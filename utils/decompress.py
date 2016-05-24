f4aea1eb90a5fd4ff6da5554b79bdd8142945501
lightless <lightless@lightless-pcdeMacBook-Pro.local>
2016-05-24 11:33:21 +0800

    完成decompress类

---
+++ utils/decompress.py
@@ -0,0 +1,118 @@
+# coding: utf-8
+
+import os
+import shutil
+import zipfile
+import tarfile
+
+import rarfile
+import magic
+
+
+"""example
+
+Decompress a compressed file.
+It will store the file in the "uploads/filename/" directory.
+for example, if there is a file named "111_test.rar" in uploads dir, after decompress,
+it will create a directory named "111_test"
+
+# import class
+from utils.decompress import Decompress
+
+# load an compressed file. only tar.gz, rar, zip supported.
+dc = Decompress('222_test.tar.gz')
+# decompress it.
+dc.decompress()
+"""
+
+
+class Decompress:
+    """Decompress zip, rar and tar.gz
+
+    filename: filename without path
+    filetype: file type display in MIME type.
+    filepath: filename with path
+
+    """
+
+    filename = None
+    filetype = None
+    filepath = None
+
+    def __init__(self, filename):
+        """
+        :param filename: a file name without path.
+        """
+        self.filename = filename
+        self.filepath = 'uploads/' + filename
+        self.filetype = magic.from_file(self.filepath, mime=True)
+        self.dir_name = os.path.splitext(self.filename)[0]
+
+    def decompress(self):
+        """
+        Decompress a file.
+        :return: a dict.
+            code: -1 -> error, 1-> success
+            msg: None or error message
+            dir_name: decompressed directory name. Usually, the directory name is the filename without file extension.
+        """
+        if self.filetype == 'application/zip':
+            return self.__decompress_zip()
+        elif self.filetype == 'application/x-rar':
+            return self.__decompress_rar()
+        elif self.filetype == 'application/x-gzip':
+            return self.__decompress_tar_gz()
+        else:
+            return {'code': -1, 'msg': u'File type error, only zip, rar, tar.gz accepted.'}
+
+    def get_file_type(self):
+        return self.filetype
+
+    def __decompress_zip(self):
+        """unzip a file."""
+        zip_file = zipfile.ZipFile(self.filepath)
+        # check if there is a filename directory
+        self.__check_filename_dir()
+
+        # create the file directory to store the extract file.
+        os.mkdir('uploads/' + self.dir_name)
+
+        zip_file.extractall('uploads/' + self.dir_name)
+        zip_file.close()
+
+        return {'code': 1, 'msg': u'Success', 'dir_name': self.dir_name}
+
+    def __decompress_rar(self):
+        """extract a rar file."""
+        rar_file = rarfile.RarFile(self.filepath)
+        # check if there is a filename directory
+        self.__check_filename_dir()
+
+        os.mkdir('uploads/' + self.dir_name)
+
+        rar_file.extractall('uploads/' + self.dir_name)
+        rar_file.close()
+
+        return {'code': 1, 'msg': u'Success', 'dir_name': self.dir_name}
+
+    def __decompress_tar_gz(self):
+        """extract a tar.gz file"""
+        tar_file = tarfile.open(self.filepath)
+        # check if there is a filename directory
+        self.__check_filename_dir()
+
+        os.mkdir('uploads/' + self.dir_name)
+
+        tar_file.extractall('uploads/' + self.dir_name)
+        tar_file.close()
+
+        return {'code': 1, 'msg': u'Success', 'dir_name': self.dir_name}
+
+    def __check_filename_dir(self):
+        if os.path.isdir('uploads/' + self.dir_name):
+            shutil.rmtree('uploads/' + self.dir_name)
+
+    def __repr__(self):
+        return "<decompress - %r>" % self.filename
+
+
