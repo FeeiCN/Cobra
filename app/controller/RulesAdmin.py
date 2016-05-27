87f844f2321d7fa4567ab603096b043c81737316
lightless <lightless@foxmail.com>
2016-05-27 10:52:09 +0800

    增加规则管理路由框架

--- app/controller/RulesAdmin.py
+++ app/controller/RulesAdmin.py
@@ -2,19 +2,46 @@
 
 from flask import render_template
 
-from app import web
+from app import web, CobraRules
 
 # default admin url
-admin_url = '/admin'
+ADMIN_URL = '/admin'
 
 
-@web.route(admin_url+'/', methods=['GET'])
-@web.route(admin_url+'/index', methods=['GET'])
+@web.route(ADMIN_URL + '/', methods=['GET'])
+@web.route(ADMIN_URL + '/index', methods=['GET'])
 def index():
-    return 'admin/index'
+    return 'admin/index - todo: login page'
 
 
-@web.route(admin_url+'/rules', methods=['GET'])
+# main view
+@web.route(ADMIN_URL + '/main', methods=['GET'])
+def main():
+    return render_template("rulesadmin/main.html")
+
+
+# all rules button
+@web.route(ADMIN_URL + '/rules', methods=['GET'])
 def rules():
-    return render_template('rulesadmin/index.html')
+    # cobra_rules = CobraRules.query.paginate(1, per_page=5, error_out=False)
+    cobra_rules = CobraRules.query.all()
+    data = {
+        # 'paginate': cobra_rules,
+        'rules': cobra_rules,
+    }
+
+    return render_template('rulesadmin/rules.html', data=data)
+
+
+# add new rules button
+@web.route(ADMIN_URL + '/add_new', methods=['GET'])
+def add_new():
+    return 'admin/add_new'
+
+
+# api: get all rules count
+@web.route(ADMIN_URL + '/all_rules_count', methods=['GET'])
+def all_rules_count():
+    rules_count = CobraRules.query.count()
+    return str(rules_count)
 
