35b98266243c88b2591af268a92894aa187c0fbf
lightless <lightless@foxmail.com>
2016-06-07 16:40:00 +0800

    修改flask-migrate设置,增加列类型检测.

--- migrations/env.py
+++ migrations/env.py
@@ -73,6 +73,7 @@ def run_migrations_online():
     context.configure(connection=connection,
                       target_metadata=target_metadata,
                       process_revision_directives=process_revision_directives,
+                      compare_type=True,
                       **current_app.extensions['migrate'].configure_args)
 
     try:
