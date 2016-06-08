479e607bfce7653296a32a0e687897c384394576
lightless <lightless@foxmail.com>
2016-06-08 16:25:27 +0800

    增加后台管理用户的model

--- app/models.py
+++ app/models.py
@@ -13,7 +13,7 @@
 #
 
 from sqlalchemy.dialects.mysql import TINYINT, INTEGER, SMALLINT
-
+from werkzeug.security import check_password_hash, generate_password_hash
 from app import db
 
 
@@ -212,3 +212,37 @@ class CobraAuth(db.Model):
 
     def __repr__(self):
         return "<CobraAuth %r-%r>" % (self.name, self.key)
+
+
+class CobraAdminUser(db.Model):
+    """
+    :role: 1-super admin, 2-admin, 3-rule admin
+    """
+    __tablename__ = 'adminuser'
+
+    id = db.Column(INTEGER(unsigned=True), primary_key=True, autoincrement=True, nullable=None)
+    username = db.Column(db.String(64), nullable=True, default=None, unique=True)
+    password = db.Column(db.String(256), nullable=True, default=None)
+    role = db.Column(TINYINT(2), nullable=True, default=None)
+    last_login_time = db.Column(db.DateTime, nullable=True, default=None)
+    last_login_ip = db.Column(db.String(16), nullable=True, default=None)
+    created_at = db.Column(db.DateTime, nullable=True, default=None)
+    updated_at = db.Column(db.DateTime, nullable=True, default=None)
+
+    def __init__(self, username, password, role, last_login_time, last_login_ip, created_at, updated_at):
+        self.username = username
+        self.generate_password(password)
+        self.role = role
+        self.last_login_time = last_login_time
+        self.last_login_ip = last_login_ip
+        self.created_at = created_at
+        self.updated_at = updated_at
+
+    def __repr__(self):
+        return "<CobraAdminUser %r-%r>" % (self.username, self.role)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def generate_password(self, password):
+        self.password = generate_password_hash(password)
