999fc5df72924fae01e1214b92762545f4727373
wufeifei <wufeifei@wufeifei.com>
2016-11-24 17:43:57 +0800

    improves something else

--- test.py
+++ test.py
@@ -59,21 +59,73 @@ class Test(unittest.TestCase):
                 print(index, self.target, project_id)
 
     def test_get_all_git_projects(self):
-        project_id = 1302
+        project_id = 1749
         checked_id = []
         from app.models import db, CobraResults
         results = CobraResults.query.filter(CobraResults.project_id == project_id).order_by(CobraResults.id.asc()).all()
-        for result in results:
-            checked_id.append(result.id)
-            same_result = CobraResults.query.filter(
+        for index, result in enumerate(results):
+            if index == 0:
+                checked_id.append(result.id)
+            del_count = CobraResults.query.filter(
                 CobraResults.project_id == project_id,
                 CobraResults.rule_id == result.rule_id,
                 CobraResults.file == result.file,
                 CobraResults.line == result.line,
                 CobraResults.status == result.status,
                 CobraResults.id.notin_(checked_id)
-            ).delete()
-            db.session.commit()
+            ).delete(synchronize_session=False)
+            if del_count > 0:
+                checked_id.append(result.id)
+                db.session.commit()
+
+    def test_hard_coded_password(self):
+        import os
+        from app.models import CobraProjects, CobraResults
+        from pickup.git import Git
+        from utils import config, common
+        projects = CobraProjects.query.order_by(CobraProjects.id.asc()).all()
+        rank = []
+        offline = []
+        for project in projects:
+            hard_coded_password_rule_ids = [137, 135, 134, 133, 132, 130, 129, 124, 123, 122]
+            count_total = CobraResults.query.filter(CobraResults.project_id == project.id, CobraResults.rule_id.in_(hard_coded_password_rule_ids)).count()
+
+            # detect project Cobra configuration file
+            if project.repository[0] == '/':
+                project_directory = project.repository
+            else:
+                project_directory = Git(project.repository).repo_directory
+            cobra_properties = config.properties(os.path.join(project_directory, 'cobra'))
+            need_scan = True
+            if 'scan' in cobra_properties:
+                need_scan = common.to_bool(cobra_properties['scan'])
+            if need_scan:
+                count_fixed = CobraResults.query.filter(CobraResults.project_id == project.id, CobraResults.rule_id.in_(hard_coded_password_rule_ids), CobraResults.status == 2).count()
+                count_not_fixed = count_total - count_fixed
+                remark = ''
+            else:
+                count_fixed = 0
+                count_not_fixed = 0
+                remark = 'offline'
+            if count_total != 0:
+                s = {
+                    'name': project.name,
+                    'id': project.id,
+                    'not_fixed': count_not_fixed,
+                    'fixed': count_fixed,
+                    'total': count_total,
+                    'remark': remark,
+                    'author': project.author
+                }
+                if s['remark'] == 'offline':
+                    offline.append(s)
+                else:
+                    rank.append(s)
+        rank = sorted(rank, key=lambda x: x['not_fixed'], reverse=True)
+        for r in rank:
+            print("| [{0}](http://cobra.meili-inc.com/report/{1}) | {6} | {2} | {3} | {4} | {5} |".format(r['name'], r['id'], r['not_fixed'], r['fixed'], r['total'], r['remark'], r['author']))
+        for r in offline:
+            print("| [{0}](http://cobra.meili-inc.com/report/{1}) | {6} | {2} | {3} | {4} | {5} |".format(r['name'], r['id'], r['not_fixed'], r['fixed'], r['total'], r['remark'], r['author']))
 
     def test_push(self):
         from daemon import push_vulnerabilities, error_handler
