16a290dcbcdfb98661f3aba127727eee84a10a71
wufeifei <wufeifei@wufeifei.com>
2016-11-25 19:13:50 +0800

    report manage

--- app/__init__.py
+++ app/__init__.py
@@ -312,3 +312,4 @@ from app.controller.backend import TasksController
 from app.controller.backend import VulsController
 from app.controller.backend import WhiteListsController
 from app.controller.backend import FramesController
+from app.controller.backend import ReportController
