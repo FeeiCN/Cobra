06720cce803133b9a450dc6ce3e38add9073aa28
wufeifei <feei@feei.cn>
2017-05-10 15:43:06 +0800

    refactor

---
+++ cobra/__init__.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    cobra
+    ~~~~~
+
+    Implements cobra main
+
+    :author:    Feei <feei@feei.cn>
+    :homepage:  https://github.com/wufeifei/cobra
+    :license:   MIT, see LICENSE for more details.
+    :copyright: Copyright (c) 2017 Feei. All rights reserved
+
+      ,---.     |
+     |    ,---.|---.,---.,---.
+    |    |   ||   ||    ,---|
+    `---``---``---``    `---^
+"""
+from .app import web, manager
+from .utils import config
+
+__version__ = '1.2.3'
+__author__ = 'Feei'
+__contact__ = 'feei@feei.cn'
+__homepage__ = 'https://github.com/wufeifei/cobra'
+__description__ = 'Cobra is a static code analysis system that automates the detecting vulnerabilities and security issue.'
+__keywords__ = 'cobra, code security'
+
+
+# - END META -
+
+def main():
+    debug = config.Config('cobra', 'debug').value
+    web.debug = bool(debug)
+    manager.run()
+
+
+if __name__ == '__main__':
+    main()
