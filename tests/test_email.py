3dd4ce2ebe825aa39b66f2bbdbd375dc9e510175
Feei <feei@feei.cn>
2017-07-31 14:00:01 +0800

    remove useless config

--- tests/test_email.py
+++ tests/test_email.py
@@ -15,6 +15,6 @@ from cobra.send_mail import send_mail
 import os
 
 
-# Fill the email settings in config.cobra first.
+# Fill the email settings in config first.
 def test_send_mail():
     assert send_mail(os.path.join(os.path.dirname(__file__), "examples/email.test"))
