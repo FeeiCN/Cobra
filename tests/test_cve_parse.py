1e62141f6d8e9e2cd83540f63dc724c2ae069586
祝荣吉 <blbana@zhurongjideMacBook-Pro.local>
2017-08-03 11:47:30 +0800

    Remove OC dependency detection and increase the dependency analysis framework

--- tests/test_cve_parse.py
+++ tests/test_cve_parse.py
@@ -1,66 +1,75 @@
 # -*- coding: utf-8 -*-
-import unittest
 import os
 import xml.etree.ElementTree as eT
 from cobra.cve_parse import CveParse
 
 
-class TestCveParse(unittest.TestCase):
-    def test_cve_parse(self):
-        cve = CveParse('../tests/examples/cve.xml', '/Users/blbana/Croba/cobra/tests/examples/pom.xml')
-        cve.cve_parse()
-        self.assertTrue('CVE-2017-9994' in cve.get_result())
+def test_cve_parse():
+    cve = CveParse('../tests/examples/cve.xml', './examples/pom.xml')
+    cve.cve_parse()
+    assert 'CVE-2017-9994' in cve.get_result()
 
-    def test_cve_info(self):
-        cve = CveParse('../tests/examples/cve.xml', '/Users/blbana/Croba/cobra/tests/examples/pom.xml')
-        tree = cve.parse_xml(cve.cve_file)
-        root = tree.getroot()
-        childs = root.iter('%sentry' % cve.NS)
-        for child in childs:  # child is entry Element
-            cve_info = cve.cve_info(child)
-        self.assertTrue('access-complexity' in cve_info)
 
-    def test_parse_xml(self):
-        cve = CveParse('../tests/examples/cve.xml', '/Users/blbana/Croba/cobra/tests/examples/pom.xml')
-        tree = cve.parse_xml(cve.cve_file)
-        root = eT.parse('./examples/cve.xml')
-        self.assertIsInstance(tree, type(root))
+def test_cve_info():
+    test_info = {}
+    cve = CveParse('../tests/examples/cve.xml', './examples/pom.xml')
+    tree = cve.parse_xml(cve.cve_file)
+    root = tree.getroot()
+    childs = root.iter('%sentry' % cve.NS)
+    for child in childs:  # child is entry Element
+        test_info = cve.cve_info(child)
+    assert 'access-complexity' in test_info
 
-    def test_get_result(self):
-        cve = CveParse('../tests/examples/cve.xml', '/Users/blbana/Croba/cobra/tests/examples/pom.xml')
-        cve.cve_parse()
-        self.assertTrue('CVE-2017-9994' in cve.get_result())
 
-    def test_rule_xml(self):
-        cve = CveParse('../tests/examples/cve.xml', '/Users/blbana/Croba/cobra/tests/examples/pom.xml')
-        cve.rule_xml()
-        self.assertTrue(os.path.exists(cve.rule_file))
+def test_parse_xml():
+    cve = CveParse('../tests/examples/cve.xml', './examples/pom.xml')
+    tree = cve.parse_xml(cve.cve_file)
+    root = eT.parse('./examples/cve.xml')
+    assert isinstance(tree, type(root))
 
-    def test_rule_parse(self):
-        cve = CveParse('../tests/examples/cve.xml', '/Users/blbana/Croba/cobra/tests/examples/pom.xml')
-        cve.rule_parse()
-        self.assertTrue('CVE-2017-9994' in cve.get_rule())
 
-    def test_rule_info(self):
-        cve = CveParse('../tests/examples/cve.xml', '/Users/blbana/Croba/cobra/tests/examples/pom.xml')
-        tree = cve.parse_xml(cve.rule_file)
-        root = tree.getroot()
-        cves = root.iter('cve')
-        for cvea in cves:
-            rule_info = cve.rule_info(cvea)
-        self.assertTrue('cpe' in rule_info)
+def test_get_result():
+    cve = CveParse('../tests/examples/cve.xml', './examples/pom.xml')
+    cve.cve_parse()
+    assert 'CVE-2017-9994' in cve.get_result()
 
-    def test_get_rule(self):
-        cve = CveParse('../tests/examples/cve.xml', '/Users/blbana/Croba/cobra/tests/examples/pom.xml')
-        cve.rule_parse()
-        self.assertTrue('CVE-2017-9994' in cve.get_rule())
 
-    def test_scan_cve(self):
-        cve = CveParse('../tests/examples/cve.xml', '/Users/blbana/Croba/cobra/tests/examples/pom.xml')
-        cve.scan_cve()
-        self.assertTrue('log4j:2.1' in cve.get_scan_result())
+def test_rule_xml():
+    cve = CveParse('../tests/examples/cve.xml', './examples/pom.xml')
+    cve.rule_xml()
+    assert os.path.exists(cve.rule_file)
 
-    def test_get_scan_result(self):
-        cve = CveParse('../tests/examples/cve.xml', '/Users/blbana/Croba/cobra/tests/examples/pom.xml')
-        cve.scan_cve()
-        self.assertTrue('log4j:2.1' in cve.get_scan_result())
+
+def test_rule_parse():
+    cve = CveParse('../tests/examples/cve.xml', './examples/pom.xml')
+    cve.rule_parse()
+    assert 'CVE-2017-9994' in cve.get_rule()
+
+
+def test_rule_info():
+    test_info = {}
+    cve = CveParse('../tests/examples/cve.xml', './examples/pom.xml')
+    tree = cve.parse_xml(cve.rule_file)
+    root = tree.getroot()
+    cves = root.iter('cve')
+    for cvea in cves:
+        test_info = cve.rule_info(cvea)
+    assert 'cpe' in test_info
+
+
+def test_get_rule():
+    cve = CveParse('../tests/examples/cve.xml', './examples/pom.xml')
+    cve.rule_parse()
+    assert 'CVE-2017-9994' in cve.get_rule()
+
+
+def test_scan_cve():
+    cve = CveParse('../tests/examples/cve.xml', './examples/pom.xml')
+    cve.scan_cve()
+    assert 'log4j:2.1' in cve.get_scan_result()
+
+
+def test_get_scan_result():
+    cve = CveParse('../tests/examples/cve.xml', './examples/pom.xml')
+    cve.scan_cve()
+    assert 'log4j:2.1' in cve.get_scan_result()
