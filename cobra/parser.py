f9d8eaab837fdf041c3466ba2d6fd9e20a15d7d0
Feei <feei@feei.cn>
2017-08-10 15:22:21 +0800

    tests for cobra.parser

--- cobra/parser.py
+++ cobra/parser.py
@@ -11,10 +11,11 @@
     :license:   MIT, see LICENSE for more details.
     :copyright: Copyright (c) 2017 Feei. All rights reserved
 """
+from .log import logger
 from phply.phplex import lexer
 from phply.phpparse import make_parser
 
-with_lineno = True
+with_line = True
 
 
 def export(items):
@@ -22,7 +23,7 @@ def export(items):
     if items:
         for item in items:
             if hasattr(item, 'generic'):
-                item = item.generic(with_lineno=with_lineno)
+                item = item.generic(with_lineno=with_line)
             result.append(item)
     return result
 
@@ -30,11 +31,11 @@ def export(items):
 def get_all_params_parent(node_node):
     _params = []
 
-    def get_all_params(node_node):
-        if node_node[0] == 'Variable':
-            _params.append(node_node[1]['name'])
+    def get_all_params(nn):
+        if nn[0] == 'Variable':
+            _params.append(nn[1]['name'])
         else:
-            for k, v in node_node[1].items():
+            for k, v in nn[1].items():
                 if isinstance(v, tuple):
                     get_all_params(v)
 
@@ -51,15 +52,23 @@ def get_all_parameter(node_params):
 
 
 def is_controllable(expr):
-    # 0 'expr': ('ArrayOffset',)
-    # 1 {'lineno': 3, 'node': ('Variable', {'lineno': 3, 'name': '$_GET'}), 'expr': 'cmd'}
-    controllabled_params = [
+    controlled_params = [
         '$_GET',
-        '$_POST'
+        '$_POST',
+        '$_REQUEST',
+        '$_COOKIE',
+        '$_FILES',
+        '$_SERVER',
+        '$HTTP_POST_FILES',
+        '$HTTP_COOKIE_VARS',
+        '$HTTP_REQUEST_VARS',
+        '$HTTP_POST_VARS',
+        '$HTTP_RAW_POST_DATA',
+        '$HTTP_GET_VARS'
     ]
     controllable_params = get_all_params_parent(expr[1]['node'])
     for cp in controllable_params:
-        if cp in controllabled_params:
+        if cp in controlled_params:
             return True, cp
     return False, None
 
@@ -82,39 +91,25 @@ def get_all_usage_controllable(params, nodes):
 
 
 def traversal(nodes, vul_function, vul_function_line, level=0):
+    func, params, uc = None, None, None
     for method, value in nodes:
         if method == 'FunctionCall':
             if value['name'] == vul_function and value['lineno'] == vul_function_line:
-                print('[FUNCTION] ', level, method, value['name'])
+                logger.debug('[FUNCTION] ', level, method, value['name'])
+                func = value['name']
                 params = get_all_parameter(value['params'])
-                print('[PARAMS]', params)
-                usage_controllable = get_all_usage_controllable(params, nodes)
-                print('[USAGE-CONTROLLABLE]', usage_controllable)
-                print('----------')
+                logger.debug('[PARAMS]', params)
+                uc = get_all_usage_controllable(params, nodes)
+                logger.debug('[USAGE-CONTROLLABLE]', uc)
+                logger.debug('----------')
         if 'nodes' in value:
             traversal(value['nodes'], vul_function, vul_function_line, level=level + 1)
-        print(method, value)
+        logger.debug(method, value)
+    return func, params, uc
 
 
 def scan(code_content, vul_function, vul_function_line):
     parser = make_parser()
-    all_nodes = export(parser.parse(code_content, debug=False, lexer=lexer.clone(), tracking=with_lineno))
-    print(all_nodes)
-    traversal(all_nodes, vul_function, vul_function_line)
-
-
-if __name__ == '__main__':
-    tests = [
-        (u"""<?php
-    $cmd = $_GET['cmd'];
-    system("ls" + $cmd);
-    """, u'system', 3),
-        (u"""<?php
-    $cmd_test = $_POST['cmd'];
-    exec("test" + $cmd_test);
-    """, u'exec', 3),
-    ]
-    for test in tests:
-        test_content, func, line = test
-        print(test)
-        scan(test_content, func, line)
+    all_nodes = export(parser.parse(code_content, debug=False, lexer=lexer.clone(), tracking=with_line))
+    logger.debug(all_nodes)
+    return traversal(all_nodes, vul_function, vul_function_line)
