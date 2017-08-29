84abaddd870a51e6e55ecda474879c79eab2b672
LoRexxar <lorexxar@gmail.com>
2017-08-29 15:27:48 +0800

    test log

--- cobra/log.py
+++ cobra/log.py
@@ -16,6 +16,8 @@ import sys
 import re
 import subprocess
 import logging
+import colorlog
+import time
 from logging import handlers
 
 # stream handle
@@ -24,11 +26,39 @@ from logging import handlers
 #
 logger = logging.getLogger('CobraLog')
 log_path = 'logs'
+
+
+def log(loglevel, log_name):
+    handler = colorlog.StreamHandler()
+    handler.setFormatter(
+        colorlog.ColoredFormatter(
+            fmt='%(log_color)s[%(levelname)s] [%(threadName)s] [%(asctime)s] [%(filename)s:%(lineno)d] %(message)s',
+            datefmt="%H:%M:%S",
+            log_colors={
+                'DEBUG': 'cyan',
+                'INFO': 'green',
+                'WARNING': 'yellow',
+                'ERROR': 'red',
+                'CRITICAL': 'red,bg_white',
+            },
+        )
+    )
+    f = open("log/" + log_name, 'a+')
+    handler2 = logging.StreamHandler(f)
+    formatter = logging.Formatter(
+        "[%(levelname)s] [%(threadName)s] [%(asctime)s] [%(filename)s:%(lineno)d] %(message)s")
+    handler2.setFormatter(formatter)
+    logger.addHandler(handler2)
+    logger.addHandler(handler)
+    logger.setLevel(loglevel)
+
 if os.path.isdir(log_path) is not True:
     os.mkdir(log_path, 0o755)
-logfile = os.path.join(log_path, 'cobra.log')
-fh_format = logging.Formatter("[%(asctime)s] %(levelname)s [%(lineno)s] %(message)s")
-sh_format = logging.Formatter("\r[%(asctime)s] [%(levelname)s] %(message)s", "%H:%M:%S")
+# logfile = os.path.join(log_path, 'cobra.log')
+# fh_format = logging.Formatter("[%(asctime)s] %(levelname)s [%(lineno)s] %(message)s")
+# sh_format = logging.Formatter("\r[%(asctime)s] [%(levelname)s] %(message)s", "%H:%M:%S")
+
+logfile = os.path.join(log_path, time.time()+'.log')
 
 UNICODE_ENCODING = "utf8"
 
@@ -82,154 +112,156 @@ if mswindows:
     ctypes.windll.kernel32.SetConsoleTextAttribute.restype = ctypes.wintypes.BOOL
 
 
-class ColorizingStreamHandler(logging.StreamHandler):
-    # color names to indices
-    color_map = {
-        'black': 0,
-        'red': 1,
-        'green': 2,
-        'yellow': 3,
-        'blue': 4,
-        'magenta': 5,
-        'cyan': 6,
-        'white': 7,
-    }
+# class ColorizingStreamHandler(logging.StreamHandler):
+#     # color names to indices
+#     color_map = {
+#         'black': 0,
+#         'red': 1,
+#         'green': 2,
+#         'yellow': 3,
+#         'blue': 4,
+#         'magenta': 5,
+#         'cyan': 6,
+#         'white': 7,
+#     }
+#
+#     # levels to (background, foreground, bold/intense)
+#     level_map = {
+#         logging.DEBUG: (None, 'blue', False),
+#         logging.INFO: (None, 'green', False),
+#         logging.WARNING: (None, 'yellow', False),
+#         logging.ERROR: (None, 'red', False),
+#         logging.CRITICAL: ('red', 'white', False)
+#     }
+#     csi = '\x1b['
+#     reset = '\x1b[0m'
+#     disable_coloring = False
+#
+#     @property
+#     def is_tty(self):
+#         isatty = getattr(self.stream, 'isatty', None)
+#         return isatty and isatty() and not self.disable_coloring
+#
+#     def emit(self, record):
+#         try:
+#             message = stdout_encode(self.format(record))
+#             stream = self.stream
+#
+#             if not self.is_tty:
+#                 if message and message[0] == "\r":
+#                     message = message[1:]
+#                 stream.write(message)
+#             else:
+#                 self.output_colorized(message)
+#             stream.write(getattr(self, 'terminator', '\n'))
+#
+#             self.flush()
+#         except (KeyboardInterrupt, SystemExit):
+#             raise
+#         except IOError:
+#             pass
+#         except Exception as e:
+#             self.handleError(record)
+#
+#     if not mswindows:
+#         def output_colorized(self, message):
+#             self.stream.write(message.decode('utf-8'))
+#     else:
+#         ansi_esc = re.compile(r'\x1b\[((?:\d+)(?:;(?:\d+))*)m')
+#
+#         nt_color_map = {
+#             0: 0x00,  # black
+#             1: 0x04,  # red
+#             2: 0x02,  # green
+#             3: 0x06,  # yellow
+#             4: 0x01,  # blue
+#             5: 0x05,  # magenta
+#             6: 0x03,  # cyan
+#             7: 0x07,  # white
+#         }
+#
+#         def output_colorized(self, message):
+#             parts = self.ansi_esc.split(message)
+#             write = self.stream.write
+#             h = None
+#             fd = getattr(self.stream, 'fileno', None)
+#
+#             if fd is not None:
+#                 fd = fd()
+#
+#                 if fd in (1, 2):  # stdout or stderr
+#                     h = ctypes.windll.kernel32.GetStdHandle(-10 - fd)
+#
+#             while parts:
+#                 text = parts.pop(0)
+#
+#                 if text:
+#                     write(text)
+#
+#                 if parts:
+#                     params = parts.pop(0)
+#
+#                     if h is not None:
+#                         params = [int(p) for p in params.split(';')]
+#                         color = 0
+#
+#                         for p in params:
+#                             if 40 <= p <= 47:
+#                                 color |= self.nt_color_map[p - 40] << 4
+#                             elif 30 <= p <= 37:
+#                                 color |= self.nt_color_map[p - 30]
+#                             elif p == 1:
+#                                 color |= 0x08  # foreground intensity on
+#                             elif p == 0:  # reset to default color
+#                                 color = 0x07
+#                             else:
+#                                 pass  # error condition ignored
+#
+#                         ctypes.windll.kernel32.SetConsoleTextAttribute(h, color)
+#
+#     def colorize(self, message, record):
+#         if record.levelno in self.level_map and self.is_tty:
+#             bg, fg, bold = self.level_map[record.levelno]
+#             params = []
+#
+#             if bg in self.color_map:
+#                 params.append(str(self.color_map[bg] + 40))
+#
+#             if fg in self.color_map:
+#                 params.append(str(self.color_map[fg] + 30))
+#
+#             if bold:
+#                 params.append('1')
+#
+#             if params and message:
+#                 if message.lstrip() != message:
+#                     prefix = re.search(r"\s+", message).group(0)
+#                     message = message[len(prefix):]
+#                 else:
+#                     prefix = ""
+#
+#                 message = "%s%s" % (prefix, ''.join((self.csi, ';'.join(params),
+#                                                      'm', message, self.reset)))
+#
+#         return message
+#
+#     def format(self, record):
+#         message = logging.StreamHandler.format(self, record)
+#         return self.colorize(message, record)
 
-    # levels to (background, foreground, bold/intense)
-    level_map = {
-        logging.DEBUG: (None, 'blue', False),
-        logging.INFO: (None, 'green', False),
-        logging.WARNING: (None, 'yellow', False),
-        logging.ERROR: (None, 'red', False),
-        logging.CRITICAL: ('red', 'white', False)
-    }
-    csi = '\x1b['
-    reset = '\x1b[0m'
-    disable_coloring = False
 
-    @property
-    def is_tty(self):
-        isatty = getattr(self.stream, 'isatty', None)
-        return isatty and isatty() and not self.disable_coloring
+# try:
+#     sh = ColorizingStreamHandler(sys.stdout)
+# except ImportError:
+#     sh = logging.StreamHandler(sys.stdout)
+# sh.setFormatter(sh_format)
+# logger.addHandler(sh)
+#
+# # file handle
+# fh = handlers.RotatingFileHandler(logfile, maxBytes=(1048576 * 5), backupCount=7)
+# fh.setFormatter(fh_format)
+# fh.setLevel(logging.DEBUG)
+# logger.addHandler(fh)
+# logger.setLevel(logging.INFO)
+log(logging.INFO, logfile)
 
-    def emit(self, record):
-        try:
-            message = stdout_encode(self.format(record))
-            stream = self.stream
-
-            if not self.is_tty:
-                if message and message[0] == "\r":
-                    message = message[1:]
-                stream.write(message)
-            else:
-                self.output_colorized(message)
-            stream.write(getattr(self, 'terminator', '\n'))
-
-            self.flush()
-        except (KeyboardInterrupt, SystemExit):
-            raise
-        except IOError:
-            pass
-        except Exception as e:
-            self.handleError(record)
-
-    if not mswindows:
-        def output_colorized(self, message):
-            self.stream.write(message.decode('utf-8'))
-    else:
-        ansi_esc = re.compile(r'\x1b\[((?:\d+)(?:;(?:\d+))*)m')
-
-        nt_color_map = {
-            0: 0x00,  # black
-            1: 0x04,  # red
-            2: 0x02,  # green
-            3: 0x06,  # yellow
-            4: 0x01,  # blue
-            5: 0x05,  # magenta
-            6: 0x03,  # cyan
-            7: 0x07,  # white
-        }
-
-        def output_colorized(self, message):
-            parts = self.ansi_esc.split(message)
-            write = self.stream.write
-            h = None
-            fd = getattr(self.stream, 'fileno', None)
-
-            if fd is not None:
-                fd = fd()
-
-                if fd in (1, 2):  # stdout or stderr
-                    h = ctypes.windll.kernel32.GetStdHandle(-10 - fd)
-
-            while parts:
-                text = parts.pop(0)
-
-                if text:
-                    write(text)
-
-                if parts:
-                    params = parts.pop(0)
-
-                    if h is not None:
-                        params = [int(p) for p in params.split(';')]
-                        color = 0
-
-                        for p in params:
-                            if 40 <= p <= 47:
-                                color |= self.nt_color_map[p - 40] << 4
-                            elif 30 <= p <= 37:
-                                color |= self.nt_color_map[p - 30]
-                            elif p == 1:
-                                color |= 0x08  # foreground intensity on
-                            elif p == 0:  # reset to default color
-                                color = 0x07
-                            else:
-                                pass  # error condition ignored
-
-                        ctypes.windll.kernel32.SetConsoleTextAttribute(h, color)
-
-    def colorize(self, message, record):
-        if record.levelno in self.level_map and self.is_tty:
-            bg, fg, bold = self.level_map[record.levelno]
-            params = []
-
-            if bg in self.color_map:
-                params.append(str(self.color_map[bg] + 40))
-
-            if fg in self.color_map:
-                params.append(str(self.color_map[fg] + 30))
-
-            if bold:
-                params.append('1')
-
-            if params and message:
-                if message.lstrip() != message:
-                    prefix = re.search(r"\s+", message).group(0)
-                    message = message[len(prefix):]
-                else:
-                    prefix = ""
-
-                message = "%s%s" % (prefix, ''.join((self.csi, ';'.join(params),
-                                                     'm', message, self.reset)))
-
-        return message
-
-    def format(self, record):
-        message = logging.StreamHandler.format(self, record)
-        return self.colorize(message, record)
-
-
-try:
-    sh = ColorizingStreamHandler(sys.stdout)
-except ImportError:
-    sh = logging.StreamHandler(sys.stdout)
-sh.setFormatter(sh_format)
-logger.addHandler(sh)
-
-# file handle
-fh = handlers.RotatingFileHandler(logfile, maxBytes=(1048576 * 5), backupCount=7)
-fh.setFormatter(fh_format)
-fh.setLevel(logging.DEBUG)
-logger.addHandler(fh)
-logger.setLevel(logging.INFO)
