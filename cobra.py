e36f014e8e6dfc78554eb3a44c15ca02fafc558c
Feei <feei@feei.cn>
2017-09-18 19:55:27 +0800

    add annotation

--- cobra.py
+++ cobra.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+    cobra
+    ~~~~~
+
+    Implements cobra entry
+
+    :author:    Feei <feei@feei.cn>
+    :homepage:  https://github.com/wufeifei/cobra
+    :license:   MIT, see LICENSE for more details.
+    :copyright: Copyright (c) 2017 Feei. All rights reserved
+"""
 import re
 import sys
 
 from cobra import main
 
-
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
     sys.exit(main())
-
