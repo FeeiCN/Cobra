14298fe33cbe1c9748249941e6a3c0c0f8a0cee8
BlBana <635373043@qq.com>
2018-03-30 12:23:50 +0800

    优化了git_projects脚本，支持参数传入扫描单个/多个目标，单条/多条指定规则，输出格式，输出方式，扫描完后是否删除项目

--- cobra/__version__.py
+++ cobra/__version__.py
@@ -29,3 +29,24 @@ __epilog__ = """Usage:
   python {m} -t {tg} -f json -o http://push.to.com/api 
   python {m} -H 127.0.0.1 -P 8888
 """.format(m='cobra.py', td='tests/vulnerabilities', tg='https://github.com/ethicalhack3r/DVWA')
+
+__introduction_git__ = """
+This script can push your target to the api
+Please write cobra_ip, secret_key in config when you want to scan the specified git address
+Please write gitlab_url, private_token, cobra_ip, secret_key when you want to scan all gitlab's projects
+"""
+
+__epilog_git__ = """Usage:
+  python {m} -a
+  python {m} -a -r cvi-190001,cvi-190002
+  python {m} -a -f json -o /tmp/report.json 
+
+  python {m} -t {td}
+  python {m} -t {td},{td1}
+  python {m} -t {td},{td1} -d
+  python {m} -t {td} -r cvi-190001,cvi-190002
+  python {m} -t {td} -f json -o /tmp/report.json 
+  python {m} -t {tg} -f json -o feei@feei.cn 
+  python {m} -t {tg} -f json -o http://push.to.com/api 
+""".format(m='git_projcets.py', td='tests/vulnerabilities', td1='tests/dvwa',
+           tg='https://github.com/ethicalhack3r/DVWA')
