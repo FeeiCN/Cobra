cb351d7b76fd786287b47da71a110e764eddbb06
PSoul <psoul1@163.com>
2018-05-08 20:30:54 +0800

    Update cobra.py
    程序执行前先git pull后再执行
    fix #799

--- cobra.py
+++ cobra.py
@@ -8,15 +8,20 @@
     Implements cobra entry
 
     :author:    Feei <feei@feei.cn>
-    :homepage:  https://github.com/WhaleShark-Team/cobra
+    :homepage:  https://github.com/FeeiCN/cobra
     :license:   MIT, see LICENSE for more details.
     :copyright: Copyright (c) 2018 Feei. All rights reserved
 """
 import re
 import sys
-
-from cobra import main
+import os
 
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
-    sys.exit(main())
+    try:
+        os.system('git pull')
+    except:
+        pass
+    finally:
+        from cobra import main
+        sys.exit(main())
