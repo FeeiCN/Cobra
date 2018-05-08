90cc651965597ddf2e0648e48680c039d0999c96
PSoul <psoul1@163.com>
2018-05-08 20:33:29 +0800

    程序执行前先git pull后再执行
    fix #799

--- cobra.py
+++ cobra.py
@@ -8,7 +8,7 @@
     Implements cobra entry
 
     :author:    Feei <feei@feei.cn>
-    :homepage:  https://github.com/FeeiCN/cobra
+    :homepage:  https://github.com/WhaleShark-Team/cobra
     :license:   MIT, see LICENSE for more details.
     :copyright: Copyright (c) 2018 Feei. All rights reserved
 """
